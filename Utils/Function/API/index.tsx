31d3d4dc00c5d803e52fc23483515f487d48f796
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-05-28 17:39:09 +0900

    Feat: [추가] 메인 스테이터스 가져오는 기능 추가

--- Utils/Function/API/index.tsx
+++ Utils/Function/API/index.tsx
@@ -1,5 +1,48 @@
 import {Page} from "puppeteer";
 
+
+export const GetPlayerMainStatus = async (page:Page) =>{
+    return await page.evaluate(()=>{
+        let status:{ [key:string]:number } = {}
+        const content_middle = document.querySelector(".content_middle")
+        if(content_middle === null) {
+            return "content_middle is null"
+        }
+        const ul = content_middle.querySelector("ul")
+        if(ul === null){
+            return "main status ul is null"
+        }
+        const ab = content_middle.querySelectorAll(".ab")
+        for(let i=0;i < ab.length;i++){
+            const el = ab[i]
+            const txt = el.querySelector(".txt")
+            const value = el.querySelector(".value")
+            if(!txt || !value){
+                return "main status txt or value is null"
+            }
+            status[txt.innerHTML] = parseInt(value.innerHTML)
+        }
+        return status
+    })
+}
+
+export const GetOvrSet = async (page:Page) =>{
+    return await page.evaluate(()=>{
+        let ovr:{ [key:string]:number } = {}
+        const ovr_set = document.querySelector(".ovr_set")
+        if(ovr_set === null){
+            return "ovr_set is null"
+        }
+        const position = ovr_set.querySelectorAll(".position")
+        for(let i=0;i<position.length;i++){
+            const p = position[i]
+            const txt = p.classList[1]
+            ovr[txt] = parseInt(p.innerHTML)
+        }
+        return ovr
+    })
+}
+
 export const GetPlayerPrice = async (page:Page) =>{
     return await page.evaluate(() => {
         const view_wrap = document.querySelector(".view_wrap")
@@ -27,5 +70,138 @@ export const GetPlayerPrice = async (page:Page) =>{
         else {
             return result
         }
+    })
+}
+
+export const GetInfoWrap = async (page:Page) =>{
+    return await page.evaluate(()=>{
+        const info = {
+            card_img:"",
+            player_img:"",
+            season_big_icon:"",
+            season_icon:"",
+            name:"",
+            main_position:"",
+            pay:0,
+            profile:""
+        }
+        //우측 선수 카드 
+        const thumb = document.querySelector(".thumb")
+        if(thumb === null) {
+            return "thumb is null"
+        }
+
+        //선수 카드 배경 이미지
+        const card_back = thumb.querySelector(".card_back")
+        if(card_back === null){
+            return "card_back is null"
+        }
+        const card_back_img = card_back.querySelector("img")
+        if(card_back_img === null){
+            return "card_back_img is null"
+        }
+        else {
+            info.card_img = card_back_img.src
+        }
+
+        //선수 이미지
+        const player_img_wrap = thumb.querySelector(".img")
+        if(player_img_wrap === null){
+            return "player_img_wrap is null"
+        }
+        const player_img = player_img_wrap.querySelector("img")
+        if(player_img === null){
+            return "player_img is null"
+        }
+        else {
+            info.player_img = player_img.src
+        }
+
+        //선수 시즌 빅 아이콘
+        const big_icon_wrap = thumb.querySelector(".season")
+        if(big_icon_wrap === null){
+            return "big_icon_wrap is null"
+        }
+        const big_icon_img = big_icon_wrap.querySelector("img")
+        if(big_icon_img === null){
+            return "big_icon_img is null"
+        }
+        else {
+            info.season_big_icon = big_icon_img.src
+        }
+
+        //좌측 선수 정보
+        const info_wrap = document.querySelector(".info_wrap")
+        if(info_wrap === null){
+            return "info_wrap is null"
+        }
+
+        //선수 명
+        const info_name = info_wrap.querySelector(".info_name")
+        if(info_name === null){
+            return "info_name is null"
+        }
+        const name = info_name.querySelector(".name")
+        if(name === null){
+            return "name is null"
+        }
+        else {
+            info.name = name.innerHTML
+        }
+
+        //선수 시즌 아이콘
+        const season_img_wrap = info_name.querySelector(".season")
+        if(season_img_wrap === null){
+            return "season_img_wrap is null"
+        }
+        const season_img = season_img_wrap.querySelector("img")
+        if(season_img === null) {
+            return "season_img is null"
+        }
+        else {
+            info.season_icon = season_img.src
+        }
+
+        //선수 급여
+        const pay_side = info_name.querySelector(".pay_side")
+        if(pay_side === null){
+            return "pay_side is null"
+        }
+        else {
+            const pay = Number(pay_side.innerHTML)
+            if(isNaN(pay)){
+                info.pay = -1
+            }
+            else {
+                info.pay = pay
+            }
+            info.pay = Number(pay_side.innerHTML)
+        }
+
+        //선수 메인 포지션
+        const main_position_wrap = info_wrap.querySelector(".info_ab")
+        if(main_position_wrap === null){
+            return "main_position_wrap is null"
+        }
+        const position = main_position_wrap.querySelectorAll(".position")
+        for(let i=0;i<position.length;i++){
+            const p = position[i]
+            const txt = p.querySelector(".txt")
+            if(txt === null){
+                info.main_position += "NULL "
+            }
+            else {
+                info.main_position += `${txt.innerHTML} `
+            }
+            const value = p.querySelector(".value")
+            if(value === null){
+                info.main_position += "000"
+            }
+            else {
+                info.main_position += value.innerHTML
+            }
+        }
+
+        return info
     })
 }
--- app/api/player/[pid]/route.ts
+++ app/api/player/[pid]/route.ts
@@ -1,157 +1,49 @@
-import puppeteer, {Page} from "puppeteer";
-import {GetPlayerPrice} from "@/Utils/Function/API";
+import puppeteer from "puppeteer";
+import {GetInfoWrap, GetOvrSet, GetPlayerMainStatus, GetPlayerPrice} from "@/Utils/Function/API";
 
 function sleep(ms:number) {
     return new Promise((r) => setTimeout(r, ms));
 }
 
-/**
- * 선수 정보 = .info_wrap
- * 선수 포지션 별 ovr = .ovr_set
- * 선수 메인 status = .content_middle
- * 선수 전체 status = .content_bottom
- * 선수 시세 = .view_wrap -> #view_wrap -> #priceContent -> .header -> .add_info -> .txt -> strong
- * */
-
-const GetInfoWrap = async (page:Page) =>{
-    return await page.evaluate(()=>{
-        const info = {
-            card_img:"",
-            player_img:"",
-            season_big_icon:"",
-            season_icon:"",
-            name:"",
-            main_position:"",
-            pay:0,
-            price:0,
-            profile:"",
-            ovr:{GK:0, LB:0, SW:0, RB:0, LWB:0, CB:0, RWB:0, CDM:0, LM:0, CM:0, RM:0, CAM:0, LW:0, CF:0, RW:0, ST:0},
-        }
-        //우측 선수 카드 
-        const thumb = document.querySelector(".thumb")
-        if(thumb === null) {
-            return "thumb is null"
-        }
-        
-        //선수 카드 배경 이미지
-        const card_back = thumb.querySelector(".card_back")
-        if(card_back === null){
-            return "card_back is null"
-        }
-        const card_back_img = card_back.querySelector("img")
-        if(card_back_img === null){
-            return "card_back_img is null"
-        }
-        else {
-            info.card_img = card_back_img.src
-        }
-        
-        //선수 이미지
-        const player_img_wrap = thumb.querySelector(".img")
-        if(player_img_wrap === null){
-            return "player_img_wrap is null"
-        }
-        const player_img = player_img_wrap.querySelector("img")
-        if(player_img === null){
-            return "player_img is null"
-        }
-        else {
-            info.player_img = player_img.src
-        }
-        
-        //선수 시즌 빅 아이콘
-        const big_icon_wrap = thumb.querySelector(".season")
-        if(big_icon_wrap === null){
-            return "big_icon_wrap is null"
-        }
-        const big_icon_img = big_icon_wrap.querySelector("img")
-        if(big_icon_img === null){
-            return "big_icon_img is null"
-        }
-        else {
-            info.season_big_icon = big_icon_img.src
-        }
-        
-        //좌측 선수 정보
-        const info_wrap = document.querySelector(".info_wrap")
-        if(info_wrap === null){
-            return "info_wrap is null"
-        }
-        
-        //선수 명
-        const info_name = info_wrap.querySelector(".info_name")
-        if(info_name === null){
-            return "info_name is null"
-        }
-        const name = info_name.querySelector(".name")
-        if(name === null){
-            return "name is null"
-        }
-        else {
-            info.name = name.innerHTML
-        }
-        
-        //선수 시즌 아이콘
-        const season_img_wrap = info_name.querySelector(".season")
-        if(season_img_wrap === null){
-            return "season_img_wrap is null"
-        }
-        const season_img = season_img_wrap.querySelector("img")
-        if(season_img === null) {
-            return "season_img is null"
-        }
-        else {
-            info.season_icon = season_img.src
-        }
-        
-        //선수 메인 포지션
-        const main_position_wrap = info_wrap.querySelector(".info_ab")
-        if(main_position_wrap === null){
-            return "main_position_wrap is null"
-        }
-        const position = main_position_wrap.getElementsByClassName("position")
-        for(let i=0;i<position.length;i++){
-            const p = position[i]
-            const txt = p.querySelector(".txt")
-            if(txt === null){
-                info.main_position += "NULL "
-            }
-            else {
-                info.main_position += `${txt.innerHTML} `
-            }
-            const value = p.querySelector(".value")
-            if(value === null){
-                info.main_position += "000"
-            }
-            else {
-                info.main_position += value.innerHTML
-            }
-            
-        }
-        
-        return info
-    })
-}
-
 export async function GET(request: Request,{ params }: { params:{ [key:string]:string } }) {
     
+    const pid = params.pid
+    const pid_number = Number(pid)
+    if(isNaN(pid_number)){
+        return Response.json({
+            message:"올바르지 않는 경로입니다.",
+            content:"다시 한번 확인해주세요."
+        },{
+            status:400,
+            headers:{
+                "Content-Type":"text/html; charset=utf-8"
+            }
+        })
+    }
     const browser = await puppeteer.launch();
     const page = await browser.newPage()
     await page.goto(`https://fconline.nexon.com/DataCenter/PlayerInfo?spid=${params.pid}`)
     await sleep(1000)
-    
     const info = await GetInfoWrap(page)
-    const bp = await GetPlayerPrice(page)
-    let json = {}
-    if(typeof info !== "string"){
-        json = {
-            ...json,
-            ...info
-        }
+    if(typeof info === "string"){
+        return Response.json({
+            message:"플레이어를 찾을 수 없습니다."
+        },{
+            status:400,
+            headers:{
+                "Content-Type":"text/html; charset=utf-8"
+            }
+        })
     }
+    const bp = await GetPlayerPrice(page)
+    const ovr = await GetOvrSet(page)
+    const main_status = await GetPlayerMainStatus(page)
+    let json = {}
     json = {
-        ...json,
-        bp
+        ...info,
+        bp:bp,
+        ovr:ovr,
+        main_status
     }
     await browser.close()
     return Response.json({ 
