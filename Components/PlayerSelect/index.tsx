2e6a1d17ebceda0c800d515980b72a746f8d4b57
jeonoseung <114666481+jeonoseung@users.noreply.github.com>
2024-06-24 00:06:27 +0900

    Feat: [추가] 스크롤 시 하단에 다가가면 자동으로 다음 목록 패칭

--- Components/PlayerSelect/index.tsx
+++ Components/PlayerSelect/index.tsx
@@ -3,19 +3,18 @@ import {Player, PlayerList} from "@/Utils/Type";
 import {getPlayerList} from "@/Utils/API";
 import {useAtom, useSetAtom} from "jotai/index";
 import {squadState} from "@/Utils/Storage/Squad";
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import PlayerAdd from "@/Components/PlayerAdd";
 import {PlayerRow} from "@/Components/PlayerRow";
 
 export default function PlayerSelect(){
     
-    const { data,isLoading,fetchNextPage,hasNextPage } = useInfiniteQuery({
+    const { data,isLoading,fetchNextPage,hasNextPage,isFetching } = useInfiniteQuery({
         queryFn:({ pageParam })=>getPlayerList({ pageParam,name:"" }),
         queryKey:["player-list"],
         initialPageParam: 1,
         getNextPageParam: (lastPage) => lastPage.next,
     })
-    
     const setState_squad = useSetAtom(squadState)
     
     const close = () =>{
@@ -32,9 +31,32 @@ export default function PlayerSelect(){
         }
     }
     
+    const table = useRef<HTMLDivElement>(null)
+    const button = useRef<HTMLButtonElement>(null)
+    const check = useRef<boolean>(true)
+   
+    const TableScrollEvent = (e:React.UIEvent<HTMLDivElement>) =>{
+        if(!hasNextPage || !check.current){
+            return
+        }
+        const t = e.currentTarget
+        const t_s_height = t.scrollHeight
+        const t_s_top = t.scrollTop
+        const diff = t_s_height - t.clientHeight
+        if(diff < t_s_top) {
+            check.current = false
+            fetchNextPage().then(()=>{
+                check.current = true
+            })
+        }
+    }
     return (
         <div className={"fixed w-full h-full z-[1000] bg-white-25"} onClick={clickOutside}>
-            <div className={"max-w-screen-md m-auto w-full h-full p-4 flex flex-col gap-2 overflow-auto"}>
+            <div 
+                className={"max-w-screen-md m-auto w-full h-full p-4 flex flex-col gap-2 overflow-auto"} 
+                onScroll={TableScrollEvent} 
+                ref={table}
+            >
                 <div className={"flex justify-end"}>
                     <button className={"text-white px-4 py-1 rounded text-sm bg-red-500"} onClick={close}>
                         닫기
@@ -51,13 +73,18 @@ export default function PlayerSelect(){
                             :
                             data &&
                             data.pages.map((i)=>(
-                                i.data.players.map((j:Player)=>(
+                                i.players.map((j:Player)=>(
                                     <PlayerRow player={j} key={j.spid}/>
                                 ))
                             ))
                     }
                     <div>
-                        
+                        {
+                            hasNextPage &&
+                                <button ref={button} className={"w-full"} onClick={() => !isFetching && fetchNextPage()}>
+                                    더보기
+                                </button>
+                        }
                     </div>
                 </div>
             </div>
